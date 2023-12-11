a7733744eadce0cac2248d21576e4266149d44e9
anunda <anunda.chu@student.mahidol.edu>
2023-12-11 18:54:18 +0700

    Solve the pdf scale problem and create json file for template

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import numpy as np
+import json
 import base64
+import os
 from streamlit_drawable_canvas import st_canvas
 from PIL import Image, ImageDraw
 from pdf2image import convert_from_bytes, convert_from_path
 
-poppler_path = r'C:\Users\user\Desktop\EGBI_433_image_processing\Release-23.11.0-0\poppler-23.11.0\Library\bin'
+poppler_path = r'C:/Users/user/Desktop/EGBI_433_image_processing/Release-23.11.0-0/poppler-23.11.0/Library/bin'
 
 st.set_page_config(
     page_title="OCR Web App",
@@ -57,6 +59,10 @@ if selected_option == "Auto-extraction":
     st.write("Please upload only PDF file")
     file = st.file_uploader("Upload a file:", type=["pdf"], accept_multiple_files=True)
    
+   #Dropdown for template selection
+    template_files = [file for file in os.listdir() if file.endswith(".json")]
+    selected_template = st.selectbox("Select a template:", template_files, key="template_selection")
+
     if file is not None and len(file) > 0:
         st.write(f"You have uploaded {len(file)} file(s).")
         file = file[0]
@@ -78,6 +84,7 @@ elif selected_option == "Manual labelling":
         st.write(f"You have uploaded {len(file)} file(s).")
         for uploaded_file in file:
 
+            # Display image and allow annotation 
             if uploaded_file.type.startswith('image'):
                 image = Image.open(uploaded_file)
                 st.subheader("Please draw an annotation box") 
@@ -86,7 +93,7 @@ elif selected_option == "Manual labelling":
                     stroke_width=2,
                     stroke_color="red",
                     background_image=image,
-                    update_streamlit=True,
+                    # update_streamlit=True,
                     height=image.size[1],
                     drawing_mode="rect",
                     key="canvas",
@@ -99,25 +106,30 @@ elif selected_option == "Manual labelling":
 
                     for i, annotated_image in enumerate(annotated_images):
                         st.image(annotated_image, caption=f"Captured Image {i + 1}", use_column_width=False)
-
+                        
                         st.text(f"Array of Captured Image {i + 1}:\n{str(annotated_arrays[i].tolist())}")
-            
+
+                        # Display coordinates in the sidebar
+                        # for j, box in enumerate(boxes):
+                            # st.sidebar.text(f"Box {j + 1} - Coordinates: Left={box['left']}, Top={box['top']}, Width={box['width']}, Height={box['height']}")
+
+            # Display converted PDF and allow annotation           
             elif uploaded_file.type == "application/pdf":
                 pdf_images = convert_from_bytes(uploaded_file.read(), poppler_path=poppler_path)  
                 for i, pdf_image in enumerate(pdf_images):
-                    st.image(pdf_image, caption=f"Page {i + 1}", use_column_width=False)
-
+                   
                     st.subheader(f"Page {i + 1}: Please draw an annotation box")
                     canvas_result = st_canvas(
                         fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
                         stroke_width=2,
                         stroke_color="red",
                         background_image=pdf_image,
-                        update_streamlit=True,
-                        height=pdf_image.size[1],
+                        # update_streamlit=True,
+
+                        height=792,
                         drawing_mode="rect",
                         key=f"canvas_page_{i}",
-                        width=pdf_image.size[0],
+                        width=612,
                     )
 
                     if st.button(f"Confirm annotations for Page {i + 1}"):
@@ -125,5 +137,9 @@ elif selected_option == "Manual labelling":
                         annotated_images, annotated_arrays = capture_annotated_region(pdf_image, boxes)
 
                         for j, annotated_image in enumerate(annotated_images):
-                            st.image(annotated_image, caption=f"Captured Image {j + 1}", use_column_width=False)
+                            # st.image(annotated_image, caption=f"Captured Image {j + 1}", use_column_width=False)
                             st.text(f"Array of Captured Image {j + 1}:\n{str(annotated_arrays[j].tolist())}")
+
+                            # Display coordinates in the sidebar
+                            # for k, box in enumerate(boxes):
+                                # st.sidebar.text(f"Page {i + 1} - Box {k + 1} - Coordinates: Left={box['left']}, Top={box['top']}, Width={box['width']}, Height={box['height']}")
